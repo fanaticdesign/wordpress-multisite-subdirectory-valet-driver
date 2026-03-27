ad93f5e33335c9d84da25364b3a6c477b6171f02
Kreig Durham <kreig.e.durham@gmail.com>
2026-03-26 21:52:36 -0600

    Normalize subsite wp-login.php routing
    Include wp-login.php in URI normalization inside frontControllerPath() (same branch as wp-admin/wp-content/wp-includes).

--- WordPressMultisiteSubdirectoryValetDriver.php
+++ WordPressMultisiteSubdirectoryValetDriver.php
@@ -50,7 +50,12 @@ class WordPressMultisiteSubdirectoryValetDriver extends BasicValetDriver
 
         // If URI contains one of the main WordPress directories, and it's not a request for the Network Admin,
         // drop the subdirectory segment before routing the request
-        if ((stripos($uri, 'wp-admin') !== false || stripos($uri, 'wp-content') !== false || stripos($uri, 'wp-includes') !== false)) {
+        if (
+            stripos($uri, 'wp-admin') !== false
+            || stripos($uri, 'wp-content') !== false
+            || stripos($uri, 'wp-includes') !== false
+            || stripos($uri, 'wp-login.php') !== false
+        ) {
 
             if (stripos($uri, 'wp-admin/network') === false) {
                 $uri = substr($uri, stripos($uri, '/wp-'));
